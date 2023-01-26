01c54982c29d9d8448f480ad71a5bb8cd65784d3
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2023-01-26 21:15:36 +0300

    chore: add tests for GrpcClient and GrpcClientFactory

--- lib/__tests__/client/grpc-client.spec.ts
+++ lib/__tests__/client/grpc-client.spec.ts
@@ -1,3 +1,38 @@
-describe('', () => {
-  it('', () => {});
+/* eslint-disable no-underscore-dangle */
+
+import * as grpc from '@grpc/grpc-js';
+import * as path from 'node:path';
+
+import { GrpcClient, GrpcClientFactory } from '@client';
+import { ProtobufLoader } from '@loaders';
+import { GrpcProtocol, GrpcStatus, GrpcTlsType } from '@protocols';
+
+describe('GrpcClient', () => {
+  let client: GrpcClient;
+
+  beforeAll(async () => {
+    // @ts-ignore
+    grpc.__setSimpleServicePackageDefinition();
+
+    client = await GrpcClientFactory.create(
+      new ProtobufLoader(path.join(__dirname, '../__fixtures__/proto/v3.proto')),
+      new GrpcProtocol({ address: '10.10.10.10', tls: { type: GrpcTlsType.INSECURE } })
+    );
+  });
+
+  it('should invoke unary request', async () => {
+    const response = await client.invokeUnaryRequest(
+      {
+        service: 'simple_package.v1.SimpleService',
+        method: 'SimpleUnaryRequest',
+      },
+      { id: '962af482-13c2-4084-a1fe-4eb135378d67' }
+    );
+
+    expect(response).toStrictEqual({
+      code: GrpcStatus.OK,
+      timestamp: 0,
+      value: { id: '962af482-13c2-4084-a1fe-4eb135378d67' },
+    });
+  });
 });
